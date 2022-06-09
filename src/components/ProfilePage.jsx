93dda3f73f3f11fc724b3ea116e788b5fd837315
lbourke1234 <lbourke1234@gmail.com>
2022-06-09 08:09:29 +0100

    prettier updates

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,61 +1,45 @@
-import SideBar from "./SideBar"
-import React from "react"
-import { Container, Row, Col } from "react-bootstrap"
-import ProfileJumbotron from "./ProfileJumbotron"
-import { useEffect, useState } from "react"
-import SidebarTop from "./SidebarTop"
-import SidebarExtraSection from "./SidebarExtraSection"
-import { useParams } from "react-router-dom"
-import ExperiencesCard from "./ExperiencesCard"
-import EducationCard from "./EducationCard"
+import SideBar from './SideBar'
+import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import ProfileJumbotron from './ProfileJumbotron'
+import { useEffect, useState } from 'react'
+import SidebarTop from './SidebarTop'
+import SidebarExtraSection from './SidebarExtraSection'
+import { useParams } from 'react-router-dom'
+import ExperiencesCard from './ExperiencesCard'
+import EducationCard from './EducationCard'
 
 const ProfilePage = ({ profiledata, setprofiledata }) => {
   const [allProfiles, setAllProfiles] = useState([])
-  const [experiencesId, setExperiencesId] = useState("")
+  // const [experiencesId, setExperiencesId] = useState('')
   const [profile, setProfile] = useState()
 
   const editProfileData = (e, field) => {
     console.log(field)
     setprofiledata({
       ...profiledata,
-      [field]: e.target.value,
+      [field]: e.target.value
     })
   }
 
   const putProfileData = async () => {
     try {
-      const response = await fetch(
-        "https://linkedin-backend-sarah-leon.herokuapp.com/profile/629f094cd81f2b9bc7cde6e2",
-        {
-          method: "PUT",
-          body: JSON.stringify(profiledata),
-          // headers: {
-          //   authorization:
-          //     'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjZmYzMwMzE3YzRlMDAwMTVkN2EwODIiLCJpYXQiOjE2NTE0OTE1ODgsImV4cCI6MTY1MjcwMTE4OH0.yS8YrZCAJfbhN7ye7OAqtaTyteCbwQsztG411czMp8s',
-          //   'Content-type': 'application/json'
-          // }
-        }
-      )
+      const response = await fetch('https://linkedin-backend-sarah-leon.herokuapp.com/profile/629f094cd81f2b9bc7cde6e2', {
+        method: 'PUT',
+        body: JSON.stringify(profiledata)
+      })
 
       const data = await response.json()
 
       setprofiledata(data)
-      console.log("✅Everything went well, infos were updated!", data)
+      console.log('✅Everything went well, infos were updated!', data)
     } catch (error) {
-      console.log("❌ something went wrong: ", error)
+      console.log('❌ something went wrong: ', error)
     }
   }
 
   const fetchAllProfiles = async () => {
-    const response = await fetch(
-      "https://linkedin-backend-sarah-leon.herokuapp.com/profile",
-      {
-        // headers: {
-        //   authorization:
-        //     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjU2YmQxZGE5MDIzOTAwMTVkOTY1YzgiLCJpYXQiOjE2NTE0OTM1NzcsImV4cCI6MTY1MjcwMzE3N30.9qnwR5Y-5lsD8GLJZNjp5T6Z__FJku-we3Sn6MwRpp0",
-        // },
-      }
-    )
+    const response = await fetch('https://linkedin-backend-sarah-leon.herokuapp.com/profile')
 
     const data = await response.json()
 
@@ -64,12 +48,7 @@ const ProfilePage = ({ profiledata, setprofiledata }) => {
   }
   const fetchProfileData = async (userId) => {
     const linkToFetch = `https://linkedin-backend-sarah-leon.herokuapp.com/profile/${userId}`
-    const response = await fetch(linkToFetch, {
-      // headers: {
-      //   authorization:
-      //     "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjZmYzMwMzE3YzRlMDAwMTVkN2EwODIiLCJpYXQiOjE2NTE0OTE1ODgsImV4cCI6MTY1MjcwMTE4OH0.yS8YrZCAJfbhN7ye7OAqtaTyteCbwQsztG411czMp8s",
-      // },
-    })
+    const response = await fetch(linkToFetch, {})
 
     const data = await response.json()
 
@@ -78,58 +57,37 @@ const ProfilePage = ({ profiledata, setprofiledata }) => {
   let params = useParams()
   useEffect(() => {
     fetchAllProfiles()
-    console.log("PARAMS", params.userId)
-    if (params !== "undefined") {
+    console.log('PARAMS', params.userId)
+    if (params !== 'undefined') {
       fetchProfileData(params.userId)
     } else {
       setProfile(profiledata)
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
     <Container>
-      {console.log("this data is important:", profile)}
+      {console.log('this data is important:', profile)}
       <Row>
         <Col md={8}>
           {profile ? (
-            <ProfileJumbotron
-              profiledata={profile}
-              editprofiledata={editProfileData}
-              putprofiledata={putProfileData}
-            />
+            <ProfileJumbotron profiledata={profile} editprofiledata={editProfileData} putprofiledata={putProfileData} />
           ) : (
-            <ProfileJumbotron
-              profiledata={profiledata}
-              editprofiledata={editProfileData}
-              putprofiledata={putProfileData}
-            />
+            <ProfileJumbotron profiledata={profiledata} editprofiledata={editProfileData} putprofiledata={putProfileData} />
           )}
 
           {profile ? (
-            <ExperiencesCard
-              profiledata={profile}
-              setprofiledata={setprofiledata}
-              profileId={profiledata._id}
-            />
+            <ExperiencesCard profiledata={profile} setprofiledata={setprofiledata} profileId={profiledata._id} />
           ) : (
-            <ExperiencesCard
-              profiledata={profiledata}
-              setprofiledata={setprofiledata}
-              profileId={profiledata._id}
-            />
+            <ExperiencesCard profiledata={profiledata} setprofiledata={setprofiledata} profileId={profiledata._id} />
           )}
           <EducationCard />
         </Col>
         <Col md={4}>
           <SidebarTop />
-          <SideBar
-            data={allProfiles.slice(0, 5)}
-            heading="People also viewed"
-          />
-          <SideBar
-            data={allProfiles.slice(6, 11)}
-            heading="People you may know"
-          />
+          <SideBar data={allProfiles.slice(0, 5)} heading="People also viewed" />
+          <SideBar data={allProfiles.slice(6, 11)} heading="People you may know" />
           <SidebarExtraSection />
         </Col>
       </Row>
